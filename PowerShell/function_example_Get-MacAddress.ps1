9f918ead05ef92b378c5f885391d7e0c4f971a6f
Floris van Enter <floris@entermi.nl>
2017-11-30 15:49:07 +0100

    SQL version determination, Powershell function example

---
+++ PowerShell/function_example_Get-MacAddress.ps1
@@ -0,0 +1,24 @@
+﻿
+
+function Get-MACAddress { 
+    <#
+	.SYNOPSIS
+	 Retrieves MAC-Address
+	.DESCRIPTION
+	 Retrieves MAC-Address from each IP enabled networkdevice in a computer
+	.PARAMETER ComputerName
+	 The name of the computer to query.
+	.EXAMPLE
+	.\Get-MacAddress -ComputerName 'desktop1'
+	.EXAMPLE
+	.\Get-MacAddress -ComputerName 'server1','server2','desktop1'
+    #>
+	[CmdletBinding()]
+	Param(
+	  [Parameter(Mandatory=$True,
+                 ValueFromPipeline=$True)]
+	  $ComputerName
+    )
+
+    $ComputerName | Foreach { Get-WmiObject -Class "Win32_NetworkAdapterConfiguration" -ComputerName $_.ToString() | Where { $_.IPEnabled -eq $True } | Select PSComputerName, MACAddress, Description }
+}
